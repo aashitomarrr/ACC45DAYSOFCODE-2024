18e0e5fc4097ce2df8aa66d07915a8ae7685420f
aashitomarrr <aashiitomar01@gmail.com>
2024-09-28 14:15:17 +0530

    Add files via upload

---
+++ Day 1 - Cricket World Cup Qualifier.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+
+    scanf("%d", &score);
+
+    if (score >= 12) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
+
+
