9ad28d66bb37c6c2124bb7a45da7488ddbe32361
aashitomarrr <aashiitomar01@gmail.com>
2024-10-08 20:42:25 +0530

    Add files via upload

---
+++ Day 11 - Police and Thief.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+       
+            printf("%d\n",abs (x-y));
+        
+        
+        
+    }
+	// your code goes here
+	return 0;
+}
