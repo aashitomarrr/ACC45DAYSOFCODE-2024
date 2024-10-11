683314ba8c86a029b8aa05754ef44fd5dcc2b257
aashitomarrr <aashiitomar01@gmail.com>
2024-10-11 21:27:50 +0530

    Add files via upload

---
+++ Day 14 - Maximise the tastiness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int max(int x, int y) {
+    return (x > y) ? x : y;
+}
+
+int main() {
+    int T;  
+    scanf("%d", &T);
+    while (T--) {
+        int a, b, c, d;
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        int maxTastiness = max(a, b) + max(c, d);
+        printf("%d\n", maxTastiness);
+    }
+    
+    return 0;
+}
